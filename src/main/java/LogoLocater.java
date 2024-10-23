37abf17b97924b76ad56ad32d757cc79a5d7a768
Enzi <hashimimaiwand1@gmail.com>
2024-10-23 16:56:38 -0600

    Using ClassName To Locate the Logo of Tekschool

---
+++ src/main/java/LogoLocater.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LogoLocater {
+    public static void main(String[] args) throws InterruptedException {
+
+        ChromeDriver chromeDriver = new ChromeDriver();
+
+        chromeDriver.get("https://tek-retail-ui.azurewebsites.net/");
+
+        chromeDriver.manage().window().maximize();
+
+        Thread.sleep(1000);
+
+        By logoLocater = By.className("top-nav__logo");
+        WebElement logoElement = chromeDriver.findElement(logoLocater);
+        String logoText = logoElement.getText();
+
+        System.out.println(logoText);
+
+        chromeDriver.quit();
+
+
+
+    }
+}
