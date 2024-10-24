f614135b9eb316a636c2b7f09abe0c3407217372
Enzi <hashimimaiwand1@gmail.com>
2024-10-24 17:42:41 -0600

    User choice browser

--- src/main/java/tek_retail_app.java
+++ src/main/java/tek_retail_app.java
@@ -1,22 +1,35 @@
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
+import java.util.Scanner;
+
 public class tek_retail_app {
     public static void main(String[] args) {
 
-        ChromeDriver EdgeDriver = new ChromeDriver();
-        EdgeDriver.get("https://dev.retail.tekschool-students.com/");
-        EdgeDriver.manage().window().maximize();
+        Scanner yourscanner = new Scanner(System.in);
+        System.out.println("\nplease choose browser: ");
+        int userChoice = yourscanner.nextInt();
 
-        String pageTitle = EdgeDriver.getTitle();
-        System.out.println("\nThis is the page title: \n" + pageTitle);
+        if (userChoice == 1 ) {
+            ChromeDriver chromeDriver = new ChromeDriver();
+            chromeDriver.get("https://dev.retail.tekschool-students.com/");
+            chromeDriver.manage().window().maximize();
 
-        EdgeDriver edgeDriver = new EdgeDriver();
-        EdgeDriver.get("https://dev.retail.tekschool-students.com/");
-        EdgeDriver.manage().window().maximize();
+            String pageTitle = chromeDriver.getTitle();
+            System.out.println("\nThis is the page title: \n" + pageTitle);
 
-        String pageTitle2 = EdgeDriver.getTitle();
-        System.out.println("\nThis is the page title: \n" + pageTitle2);
+            chromeDriver.quit();
+        }
+        else if (userChoice == 2 ) {
+            EdgeDriver edgeDriver = new EdgeDriver();
+            edgeDriver.get("https://dev.retail.tekschool-students.com/");
+            edgeDriver.manage().window().maximize();
 
+            String pageTitle2 = edgeDriver.getTitle();
+            System.out.println("\nThis is the page title: \n" + pageTitle2);
+
+            edgeDriver.quit();
+        }
     }
+
 }
